7670c9f8f7a8afd9e898691fbc42a2a9e2cd51a6
Mark McDowall <mark@mcdowall.ca>
2018-01-12 18:01:27 -0800

    v3 UI

---
+++ frontend/src/Settings/UI/UISettingsConnector.js
@@ -0,0 +1,76 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
+import createSettingsSectionSelector from 'Store/Selectors/createSettingsSectionSelector';
+import { setUISettingsValue, saveUISettings, fetchUISettings } from 'Store/Actions/settingsActions';
+import { clearPendingChanges } from 'Store/Actions/baseActions';
+import UISettings from './UISettings';
+
+function createMapStateToProps() {
+  return createSelector(
+    (state) => state.settings.advancedSettings,
+    createSettingsSectionSelector('ui'),
+    (advancedSettings, sectionSettings) => {
+      return {
+        advancedSettings,
+        ...sectionSettings
+      };
+    }
+  );
+}
+
+const mapDispatchToProps = {
+  setUISettingsValue,
+  saveUISettings,
+  fetchUISettings,
+  clearPendingChanges
+};
+
+class UISettingsConnector extends Component {
+
+  //
+  // Lifecycle
+
+  componentDidMount() {
+    this.props.fetchUISettings();
+  }
+
+  componentWillUnmount() {
+    this.props.clearPendingChanges({ section: this.props.section });
+  }
+
+  //
+  // Listeners
+
+  onInputChange = ({ name, value }) => {
+    this.props.setUISettingsValue({ name, value });
+  }
+
+  onSavePress = () => {
+    this.props.saveUISettings();
+  }
+
+  //
+  // Render
+
+  render() {
+    return (
+      <UISettings
+        onInputChange={this.onInputChange}
+        onSavePress={this.onSavePress}
+        {...this.props}
+      />
+    );
+  }
+}
+
+UISettingsConnector.propTypes = {
+  section: PropTypes.string.isRequired,
+  setUISettingsValue: PropTypes.func.isRequired,
+  saveUISettings: PropTypes.func.isRequired,
+  fetchUISettings: PropTypes.func.isRequired,
+  clearPendingChanges: PropTypes.func.isRequired
+};
+
+export default connect(createMapStateToProps, mapDispatchToProps)(UISettingsConnector);
